101c25264900836ba3c7a4947136c2b70c7aa2d1
alexizsz <alexissalazar9@gmail.com>
2023-09-29 08:15:45 +0200

    update,player/playernames added

--- src/com/Alexis/diceGame/Main.java
+++ src/com/Alexis/diceGame/Main.java
@@ -1,5 +1,7 @@
 package com.Alexis.diceGame;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*Dice game
@@ -7,12 +9,28 @@ public class Main {
         *
         *MUSTS = 2 players with names, amount of dices chosen, a winner
         * TODO (may or may not be in order)
-        *  Initialize Git rep -
-        *  Introduction to game -
-        *  Ask for names -
+        *  Initialize Git rep - DONE
+        *  Introduction to game - DONE
+        *  Ask for names - DONE
         *  Ask for amount of dices -
         *  Throw dices -
         *  Show result -
         *  Announce winner! - */
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Welcome do Dice Game v.3!");
+        System.out.println("Please choose a name for Player #1: ");
+        String newPlayer = userInput.nextLine();
+        Players playerOne = new Players(newPlayer, 1);
+        System.out.println("Welcome: " + playerOne.getPlayer() + "!");
+        System.out.println("Please choose a name for Player #2: ");
+        newPlayer = userInput.nextLine();
+        Players playerTwo = new Players(newPlayer, 2);
+        System.out.println("Welcome: " + playerTwo.getPlayer() + "!");
+
+
+
     }
+
 }
---
+++ src/com/Alexis/diceGame/Players.java
@@ -0,0 +1,15 @@
+package com.Alexis.diceGame;
+
+public class Players {
+
+
+    String player;
+    int playerNr;
+    public Players(String player, int playerNr) {
+        this.player = player;
+        this.playerNr = playerNr;
+    }
+    public String getPlayer() {
+        return player;
+    }
+}
