1f09e1ce59d2b7de6b07898b6b2d51a0d3c87fac
alexizsz <alexissalazar9@gmail.com>
2023-09-29 07:43:53 +0200

    first commit-all new project

---
+++ src/com/Alexis/diceGame/Main.java
@@ -0,0 +1,18 @@
+package com.Alexis.diceGame;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Dice game
+        *Restart of whole project, first one went to deep and felt easier to start over.
+        *
+        *MUSTS = 2 players with names, amount of dices chosen, a winner
+        * TODO (may or may not be in order)
+        *  Initialize Git rep -
+        *  Introduction to game -
+        *  Ask for names -
+        *  Ask for amount of dices -
+        *  Throw dices -
+        *  Show result -
+        *  Announce winner! - */
+    }
+}
